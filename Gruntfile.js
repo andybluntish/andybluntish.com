493b9a1f4135b6d5008fca0bdd3a1265e2fbd4b9
Andy Bluntish <andy@bluntish.net>
2014-01-21 21:59:06 +1100

    Add CNAME file - custom URL for GitHub Pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -325,7 +325,8 @@ module.exports = function (grunt) {
             '.htaccess',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'CNAME'
           ]
         }]
       },
