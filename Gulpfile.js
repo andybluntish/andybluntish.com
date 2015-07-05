08480aef7fa723e2677b9a39b9bc46917d9047ea
Andy Bluntish <andy@bluntish.net>
2015-07-05 17:40:18 +1000

    Use Gulp to process assets
    Create 'styles' task:
      - compile SASS
      - concatenate files
      - Autoprefixer
      - un-css in production mode
      - minify stylesheets in production mode
    
    Use Normalize.css with OpenType support as base for site styles.

---
+++ Gulpfile.js
@@ -0,0 +1,30 @@
+/* node: true */
+'use strict';
+
+var env = (process.env.NODE_ENV || 'development').toLowerCase();
+var paths = {
+  src: 'app',
+  dest: 'dist',
+  vendor: 'vendor'
+};
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var path = require('path');
+var glob = require('glob');
+
+gulp.task('styles', function() {
+  return gulp.src(path.join(paths.src, 'css', 'main.scss'))
+    .pipe($.sass({
+      style: 'expanded',
+      precision: 10,
+      errLogToConsole: true
+    }))
+    .pipe($.concat('main.css'))
+    .pipe($.if(env === 'production', $.uncss({
+      html: glob.sync(path.join(paths.dest, '**', '*.html'))
+    })))
+    .pipe($.autoprefixer())
+    .pipe($.if(env === 'production', $.minifyCss()))
+    .pipe(gulp.dest(path.join(paths.dest, 'css')));
+});
