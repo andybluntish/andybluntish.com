4c82cfb5e98c2fc5bf6b93bdfd0f879f496c87cc
Andy Bluntish <andy@bluntish.net>
2015-07-05 19:22:43 +1000

    Setup dev server with Browser Sync
    Use BrowserSync to proxy to Jekyll server
    
    Watch asset files:
      - rebuild with Gulp tasks
      - reload the browser
    
    Setup 'content' Gulp task to do further processing on Jekyll output

--- Gulpfile.js
+++ Gulpfile.js
@@ -12,6 +12,14 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var path = require('path');
 var glob = require('glob');
+var browserSync = require('browser-sync').create();
+var cp = require('child_process');
+
+// Content (process dist files, after Jekyll has run)
+gulp.task('content', function() {
+  return gulp.src(path.join(paths.dest, '**', '*.{html,txt,xml}'))
+    .pipe(gulp.dest(paths.dest));
+});
 
 // Styles
 gulp.task('styles', function() {
@@ -27,7 +35,8 @@ gulp.task('styles', function() {
     })))
     .pipe($.autoprefixer())
     .pipe($.if(env === 'production', $.minifyCss()))
-    .pipe(gulp.dest(path.join(paths.dest, 'css')));
+    .pipe(gulp.dest(path.join(paths.dest, 'css')))
+    .pipe(browserSync.stream());
 });
 
 // Scripts
@@ -41,5 +50,33 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest(path.join(paths.dest, 'js')));
 });
 
+// Build
+gulp.task('build', ['content', 'styles', 'scripts']);
+
+// Serve
+gulp.task('serve', ['build'], function() {
+
+  // Start Jekyll server
+  cp.spawn('jekyll', ['serve'], { stdio: 'inherit' });
+
+  // Start BrowserSync server to proxy the Jekyll server
+  browserSync.init({
+    proxy: '0.0.0.0:4000',
+    open: false,
+    notify: false,
+    reloadOnRestart: true
+  });
+
+  // Watch SASS files, then recompile and inject new styles.
+  gulp.watch(path.join(paths.src, 'css', '**', '*.scss'), ['styles']);
+
+  // Watch JS files, then recompile and reload the browser.
+  gulp.watch(path.join(paths.src, 'js', '**', '*.js'), ['scripts', browserSync.reload]);
+
+  // Watch for changes to built content files (rebuilt by Jekyll server),
+  //   do some extra processing, then reload the browser.
+  gulp.watch(path.join(paths.dest, '**', '*.{html,txt,xml}'), ['content', browserSync.reload]);
+});
+
 // Default
-gulp.task('default', ['styles', 'scripts']);
+gulp.task('default', ['build']);
