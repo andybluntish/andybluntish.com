718c7a7c036664d20f56c18bdbf45dba37eb806f
Andy Stanford-Bluntish <andy@bluntish.net>
2020-06-24 23:05:44 +0930

    render the layout on the index page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '@/layouts/Blank'
+
+export default function Home() {
+  const classNames = ['container']
+  return (
+    <Layout classNames={classNames}>
+      <p>Hello world</p>
+    </Layout>
+  )
+}
