7ce5be241e2cde88a2222325b761ea17bef603ef
Andy Stanford-Bluntish <andy@bluntish.net>
2020-06-24 23:23:08 +0930

    store site config in config file

--- components/layouts/Blank.tsx
+++ components/layouts/Blank.tsx
@@ -1,6 +1,7 @@
 import React, { PropsWithChildren } from 'react'
 import Head from 'next/head'
-import styles from './Blank.module.css'
+import config from 'config'
+import styles from '@/layouts/Blank.module.css'
 
 export interface BlankLayoutProps {
   title?: string
@@ -14,12 +15,8 @@ export default function BlankLayout({
   classNames = [],
   children,
 }: PropsWithChildren<BlankLayoutProps>) {
-  const siteTitle = 'Andy Stanford-Bluntish'
-  const siteDescription = 'I build things on the web. I love what I do.'
-  const siteAuthor = 'Andy Stanford-Bluntish'
-  const copyrightDate = new Date().getFullYear()
-  const pageTitle = (title && `${siteTitle} | ${title}`) || siteTitle
-  const pageDescription = description || siteDescription
+  const pageTitle = (title && `${config.title} | ${title}`) || config.title
+  const pageDescription = description || config.description
   const pageClassNames = [styles.root, ...classNames].filter(Boolean).join(' ')
 
   return (
@@ -29,11 +26,14 @@ export default function BlankLayout({
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <title>{pageTitle}</title>
         <meta name="description" content={pageDescription} />
-        <meta name="copyright" content={`Copyright (c) 2006–${copyrightDate} ${siteAuthor}`} />
-        <meta name="author" content={siteAuthor} />
+        <meta
+          name="copyright"
+          content={`Copyright (c) ${config.copyright.start}–${config.copyright.end} ${config.author.name}`}
+        />
+        <meta name="author" content={config.author.name} />
         <meta name="theme-color" content="#ffffff" />
         <meta name="supported-color-schemes" content="light dark" />
-        <link rel="canonical" href="https://andybluntish.com/" />
+        <link rel="canonical" href={`${config.baseURL}`} />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" type="image/png" />
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
---
+++ config.ts
@@ -0,0 +1,12 @@
+export default {
+  title: 'Andy Stanford-Bluntish',
+  description: 'I build things on the web. I love what I do.',
+  author: {
+    name: 'Andy Stanford-Bluntish',
+  },
+  copyright: {
+    start: 2006,
+    end: new Date().getFullYear(),
+  },
+  baseURL: 'https://andybluntish.com/',
+}
