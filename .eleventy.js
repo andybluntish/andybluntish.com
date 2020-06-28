d7ee7c1390319484ab6bc192bf91dbd809159a37
Andy Stanford-Bluntish <andy@bluntish.net>
2020-06-28 15:02:08 +0930

    move styles into external stylesheet

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = (config) => {
   config.addLayoutAlias('base', 'layouts/base.html')
   config.addLayoutAlias('home', 'layouts/home.html')
+
+  config.addPassthroughCopy('src/*.css')
   config.addPassthroughCopy('src/img')
 
   return {
