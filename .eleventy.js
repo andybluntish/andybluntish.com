f1737de919a89b895be6765b7596fc47dd6dc51e
Andy Stanford-Bluntish <andy@bluntish.net>
2020-06-28 14:25:46 +0930

    create a Home layout template

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 module.exports = (config) => {
+  config.addLayoutAlias('base', 'layouts/base.html')
+  config.addLayoutAlias('home', 'layouts/home.html')
   config.addPassthroughCopy('src/img')
 
   return {
-    dir: { input: 'src', output: 'dist', includes: '_includes' },
+    dir: { input: 'src', output: 'dist' },
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
   }
