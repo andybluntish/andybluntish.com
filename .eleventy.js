f4c3433a95fb929c8ba838f1a7049d977a48838a
Andy Stanford-Bluntish <andy@bluntish.net>
2020-07-08 00:22:49 +0930

    minify output html with html-minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,21 @@
+const htmlmin = require('html-minifier')
+
 module.exports = (config) => {
+  config.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath.endsWith('.html')) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        minifyCSS: true,
+        minifyJS: true,
+      })
+    }
+
+    return content
+  })
+
   config.addLayoutAlias('base', 'layouts/base.html')
   config.addLayoutAlias('home', 'layouts/home.html')
 
