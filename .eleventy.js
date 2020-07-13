eb50e05c92370ef39e1f0591220e89dd0da75510
Andy Stanford-Bluntish <andy@bluntish.net>
2020-07-13 23:10:03 +0930

    render layout for individual post pages

--- .eleventy.js
+++ .eleventy.js
@@ -36,7 +36,7 @@ module.exports = (config) => {
   })
 
   config.addLayoutAlias('base', 'layouts/base.html')
-  config.addLayoutAlias('home', 'layouts/home.html')
+  config.addLayoutAlias('post', 'layouts/post.html')
 
   config.addPassthroughCopy('src/img')
   config.addPassthroughCopy('src/manifest.json')
