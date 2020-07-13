3932f7c1f1950f9cc5fb969c807390cd5e304ae8
Andy Stanford-Bluntish <andy@bluntish.net>
2020-07-13 23:30:08 +0930

    use trunc when converting millis to seconds

--- scripts/new-post.js
+++ scripts/new-post.js
@@ -7,7 +7,7 @@ if (process.argv[2]) {
   now = new Date(process.argv[2])
 }
 
-const timestamp = Math.floor(now.valueOf() / 1e3)
+const timestamp = Math.trunc(now.valueOf() / 1e3)
 const outputPath = `./src/posts/${timestamp}.md`
 const content = `
 ---
