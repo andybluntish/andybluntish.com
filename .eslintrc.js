60291795712f765c4417eaa59f5ac8ab938f539b
Andy Stanford-Bluntish <andy@bluntish.net>
2020-07-22 22:55:07 +0930

    gulpfile is a node file

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
 
   overrides: [
     {
-      files: ['.eslintrc.js', '.eleventy.js', 'ember-cli-build.js', 'scripts/*.js'],
+      files: ['.eslintrc.js', '.eleventy.js', 'gulpfile.js', 'scripts/*.js'],
       parserOptions: {
         sourceType: 'script',
       },
