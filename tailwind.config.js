726deca647f4313bb7322d0b8a573bc78f195459
Andy Stanford-Bluntish <andy@bluntish.net>
2020-10-11 18:08:29 +1030

    extend tailwind with custom classes for theming
    Setup custom text and background classes that use css custom properties
    as their values. This will enable automatically switching theme based on
    prefers-color-scheme media queries.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
+  theme: {
+    extend: {
+      textColor: {
+        primary: 'var(--foreground)',
+        accent: 'var(--accent)',
+      },
+      backgroundColor: {
+        primary: 'var(--background)',
+      },
+    },
+  },
   purge: {
     preserveHtmlElements: false,
     content: ['./src/**/*.html', './src/**/*.njk', './src/**/*.md'],
