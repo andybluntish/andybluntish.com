ff5c4b184b8589c8e0d65f04f3bce098812cfc8b
Andy Stanford-Bluntish <andy@bluntish.net>
2020-10-20 21:40:30 +1030

    move accent general color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,11 @@ module.exports = {
         light: { raw: '(prefers-color-scheme: light)' },
         dark: { raw: '(prefers-color-scheme: dark)' },
       },
+      colors: {
+        accent: 'var(--accent)',
+      },
       textColor: {
         primary: 'var(--foreground)',
-        accent: 'var(--accent)',
       },
       backgroundColor: {
         primary: 'var(--background)',
