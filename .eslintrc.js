289588c990886b38376fe235bfb6875df69b3e69
Andy Stanford-Bluntish <andy@bluntish.net>
2020-11-03 21:04:37 +1030

    update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,29 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es2020: true,
+    commonjs: true,
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:node/recommended'],
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 2020,
   },
   rules: {
     'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
-
   overrides: [
     {
-      files: [
-        '.eslintrc.js',
-        '.eleventy.js',
-        'postcss.config.js',
-        'scripts/*.js',
-        'lib/**/*.js',
-        'tests/**/*.js',
-        'src/_data/**/*.js',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
+      files: ['tests/**/*.test.js'],
       env: {
-        node: true,
+        jest: true,
       },
-      plugins: ['node'],
     },
     {
-      files: ['tests/**/*.test.js'],
-      globals: {
-        jest: 'readonly',
-        describe: 'readonly',
-        test: 'readonly',
-        expect: 'readonly',
+      files: ['src/js/**/*.js'],
+      env: {
+        browser: true,
       },
+      extends: ['eslint:recommended'],
     },
   ],
 }
--- scripts/new-note.js
+++ scripts/new-note.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 const fs = require('fs')
 
 let now = new Date()
