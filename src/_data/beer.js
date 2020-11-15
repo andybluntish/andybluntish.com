9da71354a818920bf2c1d9e00dc4c123dd3db6c1
Andy Stanford-Bluntish <andy@bluntish.net>
2020-11-15 13:21:30 +1030

    add Recipe class

--- src/_data/beer.js
+++ src/_data/beer.js
@@ -1,39 +1,15 @@
-const CacheAsset = require('@11ty/eleventy-cache-assets')
 const Batch = require('../../lib/beer/batch')
-const fetchAllPages = require('../../lib/fetch-all-pages')
+const Recipe = require('../../lib/beer/recipe')
 
 module.exports = async function () {
   const pageSize = 50
   const batches = await Batch.fetchAll(pageSize)
-  const recipes = await fetchAllPages(fetchRecipes, pageSize)
+  const recipes = await Recipe.fetchAll(pageSize)
 
   return {
     title: 'Beer',
     description: 'Beer is for fun.',
     recipes,
     batches,
-  }
-}
-
-async function fetchRecipes(offset = 0, limit = 50) {
-  const include = ['fermentables', 'hops', 'yeasts', 'og', 'fg', 'abv', 'ibu', 'searchTags'].join(
-    ','
-  )
-  const url = `https://api.brewfather.app/v1/recipes?limit=${limit}&offset=${offset}&include=${include}`
-
-  try {
-    return CacheAsset(url, {
-      duration: '1d',
-      type: 'json',
-      fetchOptions: {
-        headers: {
-          Authorization: `Basic ${process.env.BREW_FATHER_TOKEN}`,
-        },
-      },
-    })
-  } catch (err) {
-    console.error(err)
-
-    return []
   }
 }
