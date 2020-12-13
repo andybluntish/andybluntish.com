f75ee76c7a0cb66b2495e1f2c4d2fe4b455ae82f
Andy Stanford-Bluntish <andy@bluntish.net>
2020-12-13 12:35:21 +1030

    return to CSS custom properties for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,15 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  darkMode: 'class',
+  darkMode: 'media',
   theme: {
     colors: {
       current: 'currentColor',
       gray: colors.trueGray,
-      foreground: {
-        DEFAULT: '#363d49',
-        dark: '#d1d6e0',
-      },
-      background: {
-        DEFAULT: '#fff',
-        dark: '#1b1d22',
-      },
-      accent: {
-        DEFAULT: '#d93f00',
-        dark: '#ff9e66',
-      },
-      deemphasised: {
-        DEFAULT: '#727780',
-        dark: '#81858c',
-      },
+      foreground: 'var(--foreground)',
+      background: 'var(--background)',
+      accent: 'var(--accent)',
+      deemphasised: 'var(--deemphasised)',
     },
     extend: {
       gridTemplateRows: {
@@ -29,6 +17,12 @@ module.exports = {
       },
       gridTemplateColumns: {
         layout: 'calc(50vw - 19rem) auto calc(50vw - 19rem)',
+      },
+      textColor: {
+        primary: 'var(--foreground)',
+      },
+      backgroundColor: {
+        primary: 'var(--background)',
       },
     },
   },
