ac6648156059c726d50d0495397f60c9d218d9ac
Andy Stanford-Bluntish <andy@bluntish.net>
2021-03-20 11:30:08 +1030

    update new note script to use KSUIDs

--- scripts/new-note.js
+++ scripts/new-note.js
@@ -1,15 +1,17 @@
 const fs = require('fs')
+const KSUID = require('ksuid')
 
 let now = new Date()
 if (process.argv[2]) {
   now = new Date(process.argv[2])
 }
 
-const timestamp = Math.trunc(now.valueOf() / 1e3)
-const outputPath = `./src/notes/${timestamp}.md`
+const id = KSUID.randomSync(now)
+const outputPath = `./src/notes/${id.string}.md`
 const content = `
 ---
-date: ${now.toISOString()}
+id: ${id.string}
+date: ${id.date.toISOString()}
 ---
 
 `.trimStart()
