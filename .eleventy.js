81655d103b3dcf4967a48b6992383f8840729513
Andy Stanford-Bluntish <andy@bluntish.net>
2021-04-02 15:38:51 +1030

    enable per-page CSS purge for inline style blocks

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ if (!isProd) {
 }
 
 const markdownIt = require('markdown-it')
+const parse5 = require('parse5')
+const { PurgeCSS } = require('purgecss')
 const htmlmin = require('html-minifier')
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
 const pluginRss = require('@11ty/eleventy-plugin-rss')
@@ -27,12 +29,44 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(pluginRss)
   eleventyConfig.addPlugin(syntaxHighlight)
 
-  eleventyConfig.addTransform('purgecss', function (content, outputPath) {
-    if (isProd && outputPath.endsWith('.html')) {
-      // get the contents of the <style> block
-      // using the content and styles, run purge css
-      // replace the contents of the <style> block with the pruged css
-      // optionally, minify the css (though this can be handled by htmlmin in the next step)
+  eleventyConfig.addTransform('purgecss', async function (content, outputPath) {
+    if (outputPath.endsWith('.html')) {
+      // parse the HTML content
+      const document = parse5.parse(content)
+
+      // get the <style> node from the <head>
+      const styleNode = document.childNodes
+        .find((node) => node.nodeName === 'html')
+        .childNodes.find((node) => node.nodeName === 'head')
+        .childNodes.find((node) => node.nodeName === 'style')
+        .childNodes.find((node) => node.nodeName === '#text')
+      // store the CSS as a string
+      const css = styleNode.value
+      // remove the CSS from the <style> node (otherwise purging will think it's being used)
+      styleNode.value = ''
+      // serialize the HTML without CSS as a string
+      const contentWithoutStyles = parse5.serialize(document)
+      // run purgecss using the content (sans-CSS) and the extracted CSS
+      const purgeCSSResult = await new PurgeCSS().purge({
+        content: [
+          {
+            raw: contentWithoutStyles,
+            extension: 'html',
+          },
+        ],
+        css: [
+          {
+            raw: css,
+          },
+        ],
+      })
+
+      // set the <style> node content to the purged CSS string
+      styleNode.value = purgeCSSResult[0].css
+
+      // return the serialized document as the content for this page
+      // the <style> node's content now includes only the CSS used in the HTML of the page
+      return parse5.serialize(document)
     }
 
     return content
