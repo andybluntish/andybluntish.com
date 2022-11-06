4a668607b69b2f3e1780b2205719a3da7fb100f5
Andy Stanford-Bluntish <andy@bluntish.net>
2022-11-06 12:27:56 +1030

    use symbol/use for svg footer icons

---
+++ src/_data/externalLinks.js
@@ -0,0 +1,33 @@
+const site = require("./site")();
+
+module.exports = function () {
+  const links = [
+    {
+      title: "Twitter",
+      href: "https://twitter.com/andybluntish/",
+      footer: true,
+    },
+    {
+      title: "CodePen",
+      href: "https://codepen.io/andybluntish/",
+      footer: true,
+    },
+    {
+      title: "GitHub",
+      href: "https://github.com/andybluntish/",
+      footer: true,
+    },
+    {
+      title: "Instagram",
+      href: "https://instagram.com/andybluntish/",
+      footer: true,
+    },
+    {
+      title: "Email",
+      href: `mailto:${site.author.email}`,
+      footer: true,
+    },
+  ];
+
+  return links;
+};
