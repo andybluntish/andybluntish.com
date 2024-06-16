44783cd2cbffd6d18f4350f7c9b0fde2ae39d2cb
Andy Stanford-Bluntish <andy@bluntish.net>
2024-06-16 12:26:47 +0930

    fix env source path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ CURRENT_DIR=$(dirname "$(realpath "${0:A}")")
 ROOT_DIR=$(dirname "${CURRENT_DIR}")
 
 # Load environment variables
-source "${ROOT_DIR}/../.env"
+source "${ROOT_DIR}/.env"
 
 # Deploy the site from GitHub (main branch) to Cloudflare Pages
 curl -X POST "https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/${CLOUDFLARE_DEPLOY_HOOK_TOKEN}"
