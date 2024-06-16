a8e9cb664f72aa11bc902c5f075237f3ca9954bf
Andy Stanford-Bluntish <andy@bluntish.net>
2024-06-16 11:02:44 +0930

    use the full path for sourcing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 [[ ${DEBUG:-} ]] && set -x
 
 # Load environment variables
-source .env
+SCRIPT_DIR=$(dirname "$(realpath "${0:A}")")
+source "${SCRIPT_DIR}/../.env"
 
 # Deploy the site from GitHub (main branch) to Cloudflare Pages
 curl -X POST "https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/${CLOUDFLARE_DEPLOY_HOOK_TOKEN}"
